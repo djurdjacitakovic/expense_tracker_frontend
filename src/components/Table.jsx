a3bf45f30a3411b1f12747a4def2a5b1d0f284ff
djurdjacitakovic <djukacitakovic@gmail.com>
2021-11-05 11:10:21 +0100

    finish 2

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,12 +3,13 @@ import { DataGrid } from "@mui/x-data-grid";
 import style from "../styles/Table.module.css";
 
 function DataGridDemo(props) {
+  const { rows, columns } = props;
   return (
     <div id={style.tableSize}>
       <DataGrid
         className={style.tableBackground}
-        rows={props.rows}
-        columns={props.columns}
+        columns={columns}
+        rows={rows}
         pageSize={4}
         rowsPerPageOptions={[5]}
         checkboxSelection
